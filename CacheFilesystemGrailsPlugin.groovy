e44ab1fea2aba609f0be169ad3a50d8ec354f739
stevenlanders <steven.landers@gmail.com>
2014-09-03 16:46:40 -0400

    adjusting build number

--- CacheFilesystemGrailsPlugin.groovy
+++ CacheFilesystemGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import com.thoughtworks.grails.plugin.cache.FileSystemCacheManager
 
 class CacheFilesystemGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
