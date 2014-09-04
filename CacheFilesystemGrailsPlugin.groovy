03e19094c4a6c2e8c2c89fe811f5c6af130d3f74
stevenlanders <steven.landers@gmail.com>
2014-09-04 10:16:37 -0400

    adding copyright notices

--- CacheFilesystemGrailsPlugin.groovy
+++ CacheFilesystemGrailsPlugin.groovy
@@ -1,19 +1,35 @@
+/*
+ * Copyright 2014 ThoughtWorks (http://www.thoughtworks.com).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import com.thoughtworks.grails.plugin.cache.FileSystemCacheConfigLoader
 import com.thoughtworks.grails.plugin.cache.FileSystemCacheManager
 
 class CacheFilesystemGrailsPlugin {
     // the plugin version
-    def version = "0.3"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        "web-app/**",
+        "grails-app/views/**"
     ]
 
     def loadAfter = ['cache']
 
-    // TODO Fill in these fields
     def title = "Cache Filesystem Plugin" // Headline display name of the plugin
     def author = "Steven Landers"
     def authorEmail = "steven.landers@gmail.com"
@@ -32,18 +48,6 @@ A filesystem implementation of the Cache plugin
     // Details of company behind the plugin (if there is one)
     def organization = [ name: "ThoughtWorks", url: "http://www.thoughtworks.com/" ]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
 
     def doWithSpring = {
 
@@ -55,26 +59,4 @@ A filesystem implementation of the Cache plugin
 
     }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
--- test/unit/com/thoughtworks/grails/plugin/cache/FileSystemCacheSpec.groovy
+++ test/unit/com/thoughtworks/grails/plugin/cache/FileSystemCacheSpec.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 ThoughtWorks (http://www.thoughtworks.com).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.thoughtworks.grails.plugin.cache
 
 import grails.converters.JSON
@@ -41,6 +57,8 @@ class FileSystemCacheSpec extends Specification {
             cache.put("json", jsonObj)
         then:
             assert jsonObj == cache.get("json").get()
+        cleanup:
+            cache.clear()
 
     }
 
@@ -154,7 +172,6 @@ class FileSystemCacheSpec extends Specification {
                     name: CACHE_NAME,
                     directory: TEMP_DIR
             )
-            println(TEMP_DIR)
         when:
             cache.put("person", new CacheableTestClass(name:"steven",phone:"800-555-1212"))
         then:
